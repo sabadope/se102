bcfb279b14ba1e473c64c41345de068f614cdf16
Mary Rose Gadudo <159481177+Gadudo-MaryRose06@users.noreply.github.com>
2025-03-27 10:55:45 +0800

    Add files via upload

---
+++ role/MARY/dashboard/scripts.js
@@ -0,0 +1,7 @@
+function handleClick(feature) {
+        if (feature === 'Features 1') {
+            window.location.href = "../finaleval.html";
+        } else if (feature === 'Features 2') {
+            window.location.href = "page2.html";
+        }
+    }
